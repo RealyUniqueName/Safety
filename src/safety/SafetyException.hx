89e49c01964d4966e3266c818dbb4bd8b07cd092
Aleksandr Kuzmenko <alex@stablex.ru>
2020-03-22 20:49:31 +0300

    update to the latest dev Haxe

--- src/safety/SafetyException.hx
+++ src/safety/SafetyException.hx
@@ -1,5 +1,17 @@
 package safety;
 
-import haxe.Exception;
+#if (haxe >= version('4.1.0-rc.1'))
+class SafetyException extends haxe.Exception {}
+#else
+class SafetyException {
+	public var message:String;
 
-class SafetyException extends Exception {}
+	public function new(msg:String) {
+		message = msg;
+	}
+
+	public function toString():String {
+		return message;
+	}
+}
+#end
